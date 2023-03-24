1bf17070d253de57cf7b1c3b57e581066d108f2e
Dimitri LESNOFF <dimitri.lesnoff@lip6.fr>
2023-03-24 23:10:19 +0100

    Name allocation number

--- maths/allocation_number.nim
+++ maths/allocation_number.nim
@@ -1,5 +1,6 @@
-## In a multi-threaded download, this algorithmm could be used to provide
-## each worker thread with a block of non-overlapping bytes to download.
+# Non-overlapping Partitioning
+## In a multi-threaded program, this algorithmm could be used to provide
+## each worker thread with a block of non-overlapping bytes to work on.
 
 runnableExamples:
   let numBytes = 100
@@ -13,10 +14,8 @@ runnableExamples:
     80 .. 100
   ]
   
-func allocationNum(numBytes: Natural, numPartitions: Natural): seq[Slice[Natural]] =
+func allocationNum(numBytes: Natural, numPartitions: Positive): seq[Slice[Natural]] =
   ## Divide `numBytes` bytes into `numPartitions` non-overlapping partitions.
-  if numPartitions <= 0:
-    raise newException(ValueError, "numPartitions must be > 0")
   if numPartitions > numBytes:
     raise newException(ValueError, "numPartitions must be <= numBytes")
   var
@@ -33,20 +32,26 @@ when isMainModule:
   import std/unittest
 
   suite "Test `allocationNum`":
-    test "Test `allocationNum`":
+    test "Test `allocationNum` on a divisor of bytes (same partition sizes)":
       let allocations: seq[Slice[Natural]] = allocationNum(100, 5)
       check allocations == @[
-        Natural(0) .. 19,
-        Natural(20) .. 39,
-        Natural(40) .. 59,
-        Natural(60) .. 79,
-        Natural(80) .. 100
+        Natural(0) .. Natural(19),
+        Natural(20) .. Natural(39),
+        Natural(40) .. Natural(59),
+        Natural(60) .. Natural(79),
+        Natural(80) .. Natural(100)
       ]
     
-    test "`allocationNum` on 0 partitions":
-      doAssertRaises(ValueError): discard allocationNum(5, 0)
+    test "`allocationNum` on a non-divisor of bytes":
+      let allocations: seq[Slice[Natural]] = allocationNum(104, 5)
+      check allocations == @[
+        Natural(0) .. Natural(19),
+        Natural(20) .. Natural(39),
+        Natural(40) .. Natural(59),
+        Natural(60) .. Natural(79),
+        Natural(80) .. Natural(104)
+      ]
     
     test "`allocationNum` on more partitions than bytes":
       doAssertRaises(ValueError): discard allocationNum(0, 5)
-  
-  echo allocationNum(0, 5)
+      doAssertRaises(ValueError): discard allocationNum(1, 5)
