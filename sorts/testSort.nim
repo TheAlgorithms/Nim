7b59c7005e3641ae3fce8da3953f74f77499497f
Dimitri LESNOFF <dimitri.lesnoff@lip6.fr>
2023-04-29 19:48:42 +0200

    And add those tests in an external file

---
+++ sorts/testSort.nim
@@ -0,0 +1,24 @@
+import std/[sequtils, random, algorithm]
+
+proc testSort*[T: SomeNumber](mySort: proc (x: var openArray[T]), size: Positive = 15,
+  limit: SomeNumber = 100, verbose = true): bool =
+  ## Test the sort function with a random array
+  var limit = T(limit)
+  var arr = newSeqWith(size, rand(limit))
+  if verbose:
+    echo "Before: ", arr
+  mySort(arr)
+  if verbose:
+    echo "After: ", arr
+  isSorted(arr)
+
+proc testSort*(mySort: proc (x: var openArray[char]), size: Positive = 15,
+  limit: char = 'z', verbose = true): bool =
+  ## Test the sort function with a random array
+  var arr = newSeqWith[char](size, rand('a' .. limit))
+  if verbose:
+    echo "Before: ", arr
+  mySort(arr)
+  if verbose:
+    echo "After: ", arr
+  isSorted(arr)
