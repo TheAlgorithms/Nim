5e8ef798570103f437526aec16505d21e724bf75
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-06-17 19:52:02 -0700

    Update bubble_sort.nim

--- sorting/bubble_sort.nim
+++ sorting/bubble_sort.nim
@@ -1,8 +1,15 @@
 ## Bubble Sort
 ##
+## https://en.wikipedia.org/wiki/Bubble_sort
+## Time complexity: O(n^2)
+## Auxiliary Space: O(1).
+
+## Import unit testing for testing purposes
+import unittesting
+
 ## Define function, bubble_sort()
 ## Pass in array "arr" as parameter
-proc bubble_sort(arr: var openarray[int]) =
+proc bubble_sort(arr: var openarray[int])=
   ## Optimization: if array is already sorted,
   ## it doesn't need to do this process
   var swapped = false
@@ -23,9 +30,23 @@ proc bubble_sort(arr: var openarray[int]) =
       ## if no need to make a single swap, just exit.
       return
 
+## Test
+test "Empty array":
+  var arr: seq[int]
+  bubble_sort(arr)
+  echo repr(arr)
+  check arr == []
 
-var arr = @[5, 6, 2, 1, 3]
 
-bubble_sort(arr)
+test "one element array":
+  var arr = @[1]
+  bubble_sort(arr)
+  echo repr(arr)
+  check arr == [1]
 
-echo repr(arr)
+
+test "complete array":
+  var arr = @[5, 6, 2, 1, 3]
+  bubble_sort(arr)
+  echo repr(arr)
+  check arr == @[1, 2, 3, 5, 6]
