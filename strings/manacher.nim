5bbbd432a74dcc04ca7207ccec5a43f3ab7bbbb4
Dimitri LESNOFF <dimitri.lesnoff@lip6.fr>
2023-07-07 10:16:28 +0200

    Fix indentiation with nimpretty

--- strings/manacher.nim
+++ strings/manacher.nim
@@ -19,41 +19,41 @@ runnableExamples:
   doAssert(manacherLength(example1) == 5)
 
 func manacherIndex*(s: string): HSlice[int, int] {.raises: [ValueError].} =
-    #[Longest palindrome in a string by Manacher
-      :param s: string, lowercase ascii, no whitespace
-      :returns: indexes i,j such that s[i:j] is the longest palindrome in s
-      :time complexity: O(n) where n is the string's length.
-      All the indexes refer to an intermediate string t
-      of the form "^#a#b#a#a#$" for s="abaa"
-    ]#
-    if s.len == 0:
-      raise newException(ValueError, "Empty string")
-    let extraSymbols = toHashSet(['$', '^', '#'])
-    let letters = toHashSet(s.toLowerAscii)
-    assert disjoint(extraSymbols, letters) # Forbidden letters
-    if s == "":
-        return 0 .. 1
-    let s = "^#" & join(s, "#") & "#$"
-    var
-      center = 1
-      distance = 1
-      p = repeat(0, len(s)) # Palindrome radii for each index in s
-    for index in 2 ..< len(s)-1:
-        # reflect index with respect to center
-        let mirror = 2 * center - index         # = center - (index - center)
-        p[index] = max(0, min(distance - index, p[mirror]))
-        # grow palindrome centered in i
-        while s[index + 1 + p[index]] == s[index - 1 - p[index]]:
-            p[index] += 1
-        # adjust center if necessary
-        if index + p[index] > distance:
-            center = index
-            distance = index + p[index]
-    # find the argmax index in p
-    var
-      j = maxIndex(p)
-      k = p[j]
-    return (j - k) div 2 ..< (j + k) div 2 # extract solution
+  #[Longest palindrome in a string by Manacher
+    :param s: string, lowercase ascii, no whitespace
+    :returns: indexes i,j such that s[i:j] is the longest palindrome in s
+    :time complexity: O(n) where n is the string's length.
+    All the indexes refer to an intermediate string t
+    of the form "^#a#b#a#a#$" for s="abaa"
+  ]#
+  if s.len == 0:
+    raise newException(ValueError, "Empty string")
+  let extraSymbols = toHashSet(['$', '^', '#'])
+  let letters = toHashSet(s.toLowerAscii)
+  assert disjoint(extraSymbols, letters) # Forbidden letters
+  if s == "":
+    return 0 .. 1
+  let s = "^#" & join(s, "#") & "#$"
+  var
+    center = 1
+    distance = 1
+    p = repeat(0, len(s)) # Palindrome radii for each index in s
+  for index in 2 ..< len(s)-1:
+    # reflect index with respect to center
+    let mirror = 2 * center - index # = center - (index - center)
+    p[index] = max(0, min(distance - index, p[mirror]))
+    # grow palindrome centered in i
+    while s[index + 1 + p[index]] == s[index - 1 - p[index]]:
+      p[index] += 1
+    # adjust center if necessary
+    if index + p[index] > distance:
+      center = index
+      distance = index + p[index]
+  # find the argmax index in p
+  var
+    j = maxIndex(p)
+    k = p[j]
+  return (j - k) div 2 ..< (j + k) div 2 # extract solution
 
 func manacherString*(s: string): string {.raises: [ValueError].} =
   return s[manacherIndex(s)]
