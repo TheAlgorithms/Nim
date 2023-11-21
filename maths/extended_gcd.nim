1faa670bef060aa73fa61ef43ebd02949cf79d70
vil02 <vil02@o2.pl>
2023-11-21 21:22:33 +0100

    refactor: use `uint`s as input of `euclidIteration`

--- maths/extended_gcd.nim
+++ maths/extended_gcd.nim
@@ -7,13 +7,12 @@ func updateCoefficients(t0, t1, q: int): (int, int) =
   (t1, t0 - q * t1)
 
 
-func euclidIteration(inA, inB: int): (int, int, int) =
-  assert inA >= 0 and inB >= 0
+func euclidIteration(inA, inB: uint): (uint, int, int) =
   var (a, b) = (inA, inB)
   var (x0, x1) = (1, 0)
   var (y0, y1) = (0, 1)
   while b != 0:
-    let q = a div b
+    let q = int(a div b)
     (x0, x1) = updateCoefficients(x0, x1, q)
     (y0, y1) = updateCoefficients(y0, y1, q)
     (a, b) = (b, a mod b)
@@ -21,7 +20,11 @@ func euclidIteration(inA, inB: int): (int, int, int) =
   (a, x0, y0)
 
 
-func extendedGCD*(a, b: int): (int, int, int) =
+func absAsUint(x: int): uint =
+  uint(abs(x))
+
+
+func extendedGCD*(a, b: int): (uint, int, int) =
   ## Implements the
   ## [Extended Euclidean algorithm](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm).
   ## For given integers `a`, `b` it
@@ -29,7 +32,7 @@ func extendedGCD*(a, b: int): (int, int, int) =
   ## and integers `x` and `y`, such that
   ## `gcd = a * x + b * y`
   ## Note: there are more efficient ways to compute just the `gcd`.
-  let (gcd, x, y) = euclidIteration(abs(a), abs(b))
+  let (gcd, x, y) = euclidIteration(absAsUint(a), absAsUint(b))
   return (gcd, math.sgn(a) * x, math.sgn(b) * y)
 
 
@@ -38,26 +41,26 @@ when isMainModule:
   import std/strformat
   suite "extendedGCD":
     const testCases = [
-      (10, 15, 5, -1, 1),
-      (-10, -15, 5, 1, -1),
-      (32, 64, 32, 1, 0),
-      (0, 0, 0, 0, 0),
-      (7, 0, 7, 1, 0),
-      (-8, 0, 8, -1, 0),
-      (48, 60, 12, -1, 1),
-      (98, 56, 14, -1, 2),
-      (10, -15, 5, -1, -1),
-      (997, 12345, 1, -3467, 280),
-      (997, 1234567, 1, -456926, 369),
+      (10, 15, 5u, -1, 1),
+      (-10, -15, 5u, 1, -1),
+      (32, 64, 32u, 1, 0),
+      (0, 0, 0u, 0, 0),
+      (7, 0, 7u, 1, 0),
+      (-8, 0, 8u, -1, 0),
+      (48, 60, 12u, -1, 1),
+      (98, 56, 14u, -1, 2),
+      (10, -15, 5u, -1, -1),
+      (997, 12345, 1u, -3467, 280),
+      (997, 1234567, 1u, -456926, 369),
       ].mapIt:
         let tc = (id: fmt"a={it[0]}, b={it[1]}", a: it[0], b: it[1],
             gcd: it[2], x: it[3], y: it[4])
         if tc.gcd != 0:
-          assert tc.a mod tc.gcd == 0
-          assert tc.b mod tc.gcd == 0
+          assert tc.a mod int(tc.gcd) == 0
+          assert tc.b mod int(tc.gcd) == 0
         else:
           assert tc.a == 0 and tc.b == 0
-        assert tc.gcd == tc.a * tc.x + tc.b * tc.y
+        assert int(tc.gcd) == tc.a * tc.x + tc.b * tc.y
         tc
 
     for tc in testCases:
