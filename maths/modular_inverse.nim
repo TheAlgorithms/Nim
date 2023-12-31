463491b613a54ebbc4d7fa36425a0082e67f765f
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-12-31 11:46:27 +0100

    tests: add more test cases with negative input
    Co-authored-by: dlesnoff <54949944+dlesnoff@users.noreply.github.com>

--- maths/modular_inverse.nim
+++ maths/modular_inverse.nim
@@ -32,7 +32,9 @@ when isMainModule:
   suite "modularInverse":
     const testCases = [
       (3, 7, 5),
-      (-1, 5, 4),
+      (-1, 5, 4), # Inverse of a negative
+      (-7, 5, 2), # Inverse of a negative lower than modulus
+      (-7, 4, 1), # Inverse of a negative with non-prime modulus
       (4, 5, 4),
       (9, 5, 4),
       (5, 21, 17),
