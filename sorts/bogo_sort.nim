4f7aaec7a90e1b89678f504ca24c22028c656a30
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-11 21:37:17 +0200

    style: apply nimpretty to bogo_sort.nim

--- sorts/bogo_sort.nim
+++ sorts/bogo_sort.nim
@@ -9,7 +9,7 @@ runnableExamples:
   var arr = @[3, 1, 2]
   bogoSort(arr)
   doAssert isSorted(arr)
-  
+
   var arr2 = @["c", "a", "b"]
   bogoSort(arr2)
   doAssert isSorted(arr2)
@@ -18,24 +18,24 @@ runnableExamples:
 import random
 
 func isSorted[T](arr: openArray[T]): bool =
-    for i in 0..<arr.len - 1:
-        if arr[i] > arr[i + 1]:
-            return false
-    return true
+  for i in 0..<arr.len - 1:
+    if arr[i] > arr[i + 1]:
+      return false
+  return true
 
 proc bogoSort*[T](arr: var openArray[T]) =
-    while not isSorted(arr):
-        shuffle(arr)
+  while not isSorted(arr):
+    shuffle(arr)
 
 when isMainModule:
-    import std/unittest
-    suite "BogoSortTests":
-        test "sort an array of integers":
-            var arr = @[3, 1, 2]
-            bogoSort(arr)
-            check isSorted(arr)
+  import std/unittest
+  suite "BogoSortTests":
+    test "sort an array of integers":
+      var arr = @[3, 1, 2]
+      bogoSort(arr)
+      check isSorted(arr)
 
-        test "sort an array of strings":
-            var arr = @["c", "a", "b"]
-            bogoSort(arr)
-            check isSorted(arr)
+    test "sort an array of strings":
+      var arr = @["c", "a", "b"]
+      bogoSort(arr)
+      check isSorted(arr)
